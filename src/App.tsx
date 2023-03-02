a59291c053c979c6e170aac0322c4789b3385335
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-03-02 23:49:49 +0900

    variant

--- src/App.tsx
+++ src/App.tsx
@@ -17,15 +17,17 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars = {
+  start: { scale: 0 }, // 변화 전 모습
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } }, // 변화 후 모습
+  // 변화 속도 : delay: 0.5
+  // spring : 에니메이션 끝나면서 약간 반동을 주는 것
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box
-        transition={{ type: "spring", delay: 0.5 }} // 변화 속도
-        // spring : 에니메이션 끝나면서 약간 반동을 주는 것
-        initial={{ scale: 0 }} // 변화 전 모습
-        animate={{ scale: 1, rotateZ: 360 }} // 변화 후 모습
-      />
+      <Box variants={myVars} initial="start" animate="end" />
     </Wrapper>
   );
 }
