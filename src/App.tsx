b321d035be59c10dd076d78399232a4059fb399b
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-03-02 23:15:30 +0900

    styled component + framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -8,18 +9,23 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box
+        transition={{ type: "spring", delay: 0.5 }} // 변화 속도
+        // spring : 에니메이션 끝나면서 약간 반동을 주는 것
+        initial={{ scale: 0 }} // 변화 전 모습
+        animate={{ scale: 1, rotateZ: 360 }} // 변화 후 모습
+      />
     </Wrapper>
   );
 }
